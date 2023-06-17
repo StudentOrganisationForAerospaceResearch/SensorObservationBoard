1a3533b4cd87df64ad28a719a05b6d2bf14a7d95
AgamAulakh <aulakhagam@gmail.com>
2023-06-17 12:40:48 -0600

    includes stm32f4 hal in system defines

--- Components/SystemDefines.hpp
+++ Components/SystemDefines.hpp
@@ -28,6 +28,7 @@
 
 #include "cmsis_os.h"	// CMSIS RTOS definitions
 #include "main_avionics.hpp"  // Main avionics definitions
+#include "stm32f4xx_hal.h"	 // HAL definitions
 #include "Utils.hpp"	// Utility functions
 
 /* Task Definitions ------------------------------------------------------------------*/
